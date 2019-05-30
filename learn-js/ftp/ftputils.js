e288a178e1fcb511210e427c5c323447caac4014
Satya Komatineni <satya.komatineni@gmail.com>
2019-05-30 10:13:29 -0400

    Cleanup code. Error handling added

--- learn-js/ftp/ftputils.js
+++ learn-js/ftp/ftputils.js
@@ -1,5 +1,8 @@
 
 exports.isValid = isValid
+exports.isValidObject=isValidObject
+exports.isInvalidObject = function(obj) { return !(isValidObject(obj))}
+exports.test=test
 
 exports.isInValid = function (stringValue)
 {
@@ -7,7 +10,11 @@ exports.isInValid = function (stringValue)
 }
 
 function isValid(stringValue) {
-    if (typeof(stringValue) == undefined)
+    if (stringValue == undefined)
+    {
+        return false;
+    }
+    if (stringValue == null)
     {
         return false;
     }
@@ -23,20 +30,44 @@ function isValid(stringValue) {
 }//eof-function
 
 
+function isValidObject(obj) {
+    if (obj == undefined)
+    {
+        return false;
+    }
+    if (obj == null)
+    {
+        return false;
+    }
+    return true;
+}//eof-function
+
+function a(v1, v2) {
+    if (v1 === v2)
+    {
+        console.log("passed");
+        return;
+    }
+    console.log(`test failed. ${v1}:${v2}`);
+}
 function test()
 {
     //Some tests
     let someUndefinedValue;
-    console.log(isValid(someUndefinedValue));
-
-    console.log(isValid(5));
-
-    console.log(isValid(""));
-    console.log(isValid("    "));
-
-    //proper string
-    console.log(isValid("hello"));
+    a(false, isValid(someUndefinedValue));
+    a(false, isValid(5));
+    a(false,isValid(""));
+    a(false,isValid("    "));
+    a(true,isValid("hello"));
 
     //proper string
-    console.log(isValid("  hello    "));
+    a(true,isValid("  hello    "));
+
+    //invalid null object
+    let nullObj = null;
+    a(false,isValidObject(nullObj));
+    a(false,isValidObject(someUndefinedValue));
+
 }
+
+//test();
--- learn-js/ftp/testftp.js
+++ learn-js/ftp/testftp.js
@@ -1,15 +1,16 @@
 
 const ftp = require("basic-ftp");
 const readenv = require("./readenv");
+const u = require("./ftputils");
 
 async function getNamesfromFtpLocation(ftpSiteName) {
     const client = new ftp.Client()
     client.ftp.verbose = true
-    let dirListing;
+    let dirListing = null;
     try {
         const ftpHostSpec = new readenv.createHostSpecFromEnv(ftpSiteName);
 
-        //wait to succeed to 
+        //wait to login
         console.log("waiting to connect to:" + ftpHostSpec.name);
         await client.access({
             host: ftpHostSpec.ip,
@@ -18,24 +19,61 @@ async function getNamesfromFtpLocation(ftpSiteName) {
             //,secure: true
         })
 
-        //finished logging in
+        //finished logging in, after await
         console.log("Successful login");
 
         //wait to get a list of what the site has to offer
         console.log("wait for a list of files");
         dirListing = await client.list();
-        console.log("listing of files");
-        console.log(dirListing);
+        //clsworkWithListings(dirListing);
     }
     catch(err) {
+        console.log("in the catch block of both awaits")
         console.log(err)
     }
     console.log("closing the ftp connection")
     client.close()
 
+    //dirListing may be null if the await block fails above
     return dirListing;
 }
 
 //lets call the function
 
-let dirListing  = getNamesfromFtpLocation("wu1");
+/**
+ * @param {FileInfo[]} ftpListingArray 
+ * 
+ * Each FileInfo looks like this
+ * 
+ * FileInfo {
+    name: '14122018',
+    type: 2,
+    size: 0,
+    permissions: { user: 7, group: 5, world: 5 },
+    hardLinkCount: 1,
+    link: '',
+    group: 'ftp',
+    user: 'ftp',
+    date: 'Dec 14 2018' }
+ *
+ *
+ */
+function workWithListings(ftpListingArray)
+{
+    if (u.isInvalidObject(ftpListingArray))
+    {
+        console.log(`The listing variable is not valid: ${ftpListingArray}`);
+        return;
+    }
+    console.log("Number of directories/files :" + ftpListingArray.length);
+}
+
+async function printFtpListingsFor(ftpSiteName)
+{
+    // {Promise<any>} dirListingPromise
+    // {FileInfo[]} ftpListingArray 
+    let ftpListingArray = await getNamesfromFtpLocation(ftpSiteName);
+    workWithListings(ftpListingArray);
+}
+
+printFtpListingsFor("someUnknownFtp");
